a2cb7d21b757c4ab71ad4c0efe5267d3b45c17b0
ZaidYounan <zaid.younan@gmail.com>
2022-02-09 22:44:57 +1100

    Create function that will render the knight image on the randomly chosen knightPosition

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,7 +11,7 @@ const knightPosition = {
 
 function Board() {
     let board = [];
-    console.log(knightPosition)
+
     for (let i = verticalAxis.length - 1; i >= 0; i--) {
       for (let k = 0; k < horizontalAxis.length; k++) {
 
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './stylesheets/Square.css'
-import knightimg from '../images/knight.png' 
+import knightimg from '../images/knight.png'; 
+
 
 function Square(props) {
     const {k, i, keyProp, board, knightPosition} = props;   
     const {xPosition, yPosition} = knightPosition;
     const knight = `${xPosition}${yPosition}`
 
-    
+    function knightShow() {
+        if (keyProp == knight) {
+            return knightimg
+        } else {
+            return null
+        }
+    }
+
     function tileColour(axisOne, axisTwo) {
         const number = axisOne + axisTwo + 2;
 
@@ -18,8 +26,10 @@ function Square(props) {
         }
     }
     
-  return <div className={`Square ${tileColour(k, i)}`}></div>
-  ;
+    return (
+        <div className={`Square ${tileColour(k, i)}`}>
+            <img src={knightShow()}></img>
+        </div>);
 }
 
 export default Square;
