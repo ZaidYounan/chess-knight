711bdb8957fc6110c6de6622ef12ebbddb24d52e
ZaidYounan <zaid.younan@gmail.com>
2022-02-09 18:35:27 +1100

    Make board checkered using function with conditional statement

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,12 +7,21 @@ const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
 export default function Board() {
     let board = [];
 
+    function tileColour(axisOne, axisTwo) {
+        const number = axisOne + axisTwo + 2;
+        
+        if (number % 2 === 0) {
+            return "Light-Tile"
+        } else {
+            return "Dark-Tile"
+        }
+    }
 
     for (let i = verticalAxis.length - 1; i >= 0; i--) {
       for (let k = 0; k < horizontalAxis.length; k++) {
         
         board.push(
-            <div className="Square" key={`${horizontalAxis[k]}${verticalAxis[i]}`}>{horizontalAxis[k]}{verticalAxis[i]}</div>
+            <div className={`Square ${tileColour(k, i)}`} key={`${horizontalAxis[k]}${verticalAxis[i]}`}>{horizontalAxis[k]}{verticalAxis[i]}</div>
         );
       }
     }
