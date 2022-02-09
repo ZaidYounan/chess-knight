e9bda3aa3128485b16e3891bcc8528225fe59ba9
ZaidYounan <zaid.younan@gmail.com>
2022-02-09 22:29:58 +1100

    Create logic for random knightPosition, and pass as prop to Square component

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,21 +3,24 @@ import Square from './Square';
 import './stylesheets/Board.css';
 
 const horizontalAxis= ["A", "B", "C", "D", "E", "F", "G", "H"];
-const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+const knightPosition = { 
+    xPosition: horizontalAxis[Math.floor((Math.random() * 7) + 0)],
+    yPosition: verticalAxis[Math.floor((Math.random() * 7) + 0)]
+}
 
 function Board() {
     let board = [];
-
-    
-
+    console.log(knightPosition)
     for (let i = verticalAxis.length - 1; i >= 0; i--) {
       for (let k = 0; k < horizontalAxis.length; k++) {
-        
-        board.push(<Square {...{k, i}} key={`${horizontalAxis[k]}${verticalAxis[i]}`}/>
+
+        let keyProp = `${horizontalAxis[k]}${verticalAxis[i]}`
+
+        board.push(<Square key={keyProp} {...{k, i, keyProp, board, knightPosition}}/>
         );
       }
     }
-    console.log(board)
 
     return <div className="Board">{board}</div>;
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './stylesheets/Square.css'
+import knightimg from '../images/knight.png' 
 
 function Square(props) {
-    const {k, i} = props;
+    const {k, i, keyProp, board, knightPosition} = props;   
+    const {xPosition, yPosition} = knightPosition;
+    const knight = `${xPosition}${yPosition}`
+
     
     function tileColour(axisOne, axisTwo) {
         const number = axisOne + axisTwo + 2;
